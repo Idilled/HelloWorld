c542ef5ea133792f30d17fc5d22b5bd833d93e54
Idilled <130499611+Idilled@users.noreply.github.com>
2023-12-02 16:58:30 +0400

    Задача №2_Типы данных

---
+++ src/main/java/com/max/idea/DataType.java
@@ -0,0 +1,12 @@
+package com.max.idea;
+
+import java.sql.SQLOutput;
+
+public class DataType {
+    public static void main(String[] args) {
+        double price = 1399.99;
+        byte temperature = 127;
+        System.out.println("Переменная типа с плавающей точкой с наибольшим диапазоном чисел - double. Пример значения: " + price);
+        System.out.println("Переменная целочисленного типа с наименьшим диапазоном чисел - byte. Наибольшее возможное значение: " + temperature);
+    }
+}
