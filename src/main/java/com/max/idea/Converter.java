6fe2797c3ecf55da6dbcb17e07e09f639ab15690
Idilled <130499611+Idilled@users.noreply.github.com>
2023-12-05 22:16:02 +0400

    Задача №6_Операторы

---
+++ src/main/java/com/max/idea/Converter.java
@@ -0,0 +1,110 @@
+package com.max.idea;
+
+import java.util.Scanner;
+
+public class Converter {
+    public static void main(String[] args) {
+        System.out.println("Этот конвертер физических величин позволяет переводить ряд единиц измерения массы и расстояния");
+        System.out.println("*********");
+        Scanner input = new Scanner(System.in);
+        double[] massArray = new double[4];
+        double[] distanceArray = new double[4];
+        String[] massArraytext = {"граммы", "килограммы", "унции", "пуды"};
+        String[] distanceArraytext = {"метры", "километры", "мили", "ярды"};
+        int massType = 0;
+        double massNumber = 0;
+        int distanceType = 0;
+        double distanceNumber = 0;
+
+        System.out.println("Выберите что переводить: \n1 - масса \n2 - расстояние");
+        int physicalQuantity = input.nextInt();
+
+        if (physicalQuantity == 1) {
+            System.out.println("Выберите единицу измерения: \n1 - грамм \n2 - килограмм \n3 - унция \n4 - пуд");
+            massType = input.nextInt();
+            if (massType == 1 | massType == 2 | massType == 3 | massType == 4) {
+                System.out.println("Введите количество выбранных единиц: ");
+                massNumber = input.nextDouble();
+            } else {
+                System.out.println("Пожалуйста, перезапустите программу и введите 1,2,3 или 4");
+            }
+        }
+
+        else if (physicalQuantity == 2) {
+            System.out.println("Выберите единицу измерения: \n1 - метр \n2 - километр \n3 - миля \n4 - ярд");
+            distanceType = input.nextInt();
+            if (distanceType == 1 | distanceType == 2 | distanceType == 3 | distanceType == 4) {
+                System.out.println("Введите количество выбранных единиц: ");
+                distanceNumber = input.nextDouble();
+            } else {
+                System.out.println("Пожалуйста, перезапустите программу и введите 1,2,3 или 4");
+            }
+        }
+
+        else {
+            System.out.println("Пожалуйста, перезапустите программу и введите 1 или 2");
+        }
+        if (physicalQuantity == 1) {
+            switch(massType) {
+                case 1:
+                    massArray[0] = massNumber;
+                    massArray[1] = massNumber / 1000;
+                    massArray[2] = massNumber / 28.35;
+                    massArray[3] = massNumber / 16380;
+                    break;
+                case 2:
+                    massArray[0] = massNumber * 1000;
+                    massArray[1] = massNumber;
+                    massArray[2] = massNumber * 35.274;
+                    massArray[3] = massNumber / 16.381;
+                    break;
+                case 3:
+                    massArray[0] = massNumber * 28.35;
+                    massArray[1] = massNumber / 35.274;
+                    massArray[2] = massNumber;
+                    massArray[3] = massNumber / 577.8;
+                    break;
+                case 4:
+                    massArray[0] = massNumber * 16380;
+                    massArray[1] = massNumber * 16.381;
+                    massArray[2] = massNumber * 577.8;
+                    massArray[3] = massNumber;
+                    break;
+            }
+            for (int i = 0; i < 4; i++) {
+                System.out.println(massArraytext[i] + ": " + massArray[i]);
+            }
+        } else if (physicalQuantity == 2) {
+            switch (distanceType) {
+                case 1:
+                    distanceArray[0] = distanceNumber;
+                    distanceArray[1] = distanceNumber / 1000;
+                    distanceArray[2] = distanceNumber / 1609;
+                    distanceArray[3] = distanceNumber * 1.094;
+                    break;
+                case 2:
+                    distanceArray[0] = distanceNumber * 1000;
+                    distanceArray[1] = distanceNumber;
+                    distanceArray[2] = distanceNumber / 1.609;
+                    distanceArray[3] = distanceNumber * 1094;
+                    break;
+                case 3:
+                    distanceArray[0] = distanceNumber * 1609;
+                    distanceArray[1] = distanceNumber * 1.609;
+                    distanceArray[2] = distanceNumber;
+                    distanceArray[3] = distanceNumber * 1760;
+                    break;
+                case 4:
+                    distanceArray[0] = distanceNumber / 1.094;
+                    distanceArray[1] = distanceNumber / 1094;
+                    distanceArray[2] = distanceNumber / 1760;
+                    distanceArray[3] = distanceNumber;
+                    break;
+            }
+            for (int i = 0; i < 4; i++) {
+                System.out.println(distanceArraytext[i] + ": " + distanceArray[i]);
+            }
+        }
+
+    }
+}
