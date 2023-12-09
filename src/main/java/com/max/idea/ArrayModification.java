ff39acf098d6b453a26b86649d05ffda2bccdd12
Idilled <130499611+Idilled@users.noreply.github.com>
2023-12-10 00:21:07 +0400

    Задача №9_Циклы

--- src/main/java/com/max/idea/ArrayModification.java
+++ src/main/java/com/max/idea/ArrayModification.java
@@ -7,25 +7,31 @@ public class ArrayModification {
         int arrayLength;
         double sum = 0;
         double arithMean;
+        double[] array;
         Scanner input = new Scanner(System.in);
 
         System.out.println("Введите размер массива");
         arrayLength = input.nextInt();
-        double[] array = new double[arrayLength];
+        if (arrayLength == 0) {
+            System.out.println("Размер массива 0, перезапустите программу и введите целое положительное число");
+        } else if (arrayLength < 0) {
+            System.out.println("Вы ввели отрицательное число, перезапустите программу и введите целое положительное число");
+        } else {
+            array = new double[arrayLength];
+            System.out.println("Введите элементы массива, их должно быть " + arrayLength);
+            for (int i = 0; i < arrayLength; i++) {
+                array[i] = input.nextDouble();
+            }
 
-        System.out.println("Введите элементы массива, их должно быть " + arrayLength);
-        for (int i = 0; i < arrayLength; i++) {
-            array[i] = input.nextDouble();
-        }
+            for (int i = 0; i < arrayLength; i++) {
+                sum += array[i];
+            }
+            arithMean = sum / arrayLength;
 
-        for (int i = 0; i < arrayLength; i++) {
-            sum += array[i];
-        }
-        arithMean = sum / arrayLength;
-
-        System.out.println("Массив, где каждый элемент умножается на среднее арифметическое");
-        for (int i = 0; i < arrayLength; i++) {
-            System.out. println (array[i] * arithMean);
+            System.out.println("Массив, где каждый элемент умножается на среднее арифметическое");
+            for (int i = 0; i < arrayLength; i++) {
+                System.out.println(array[i] * arithMean);
+            }
         }
     }
 }
