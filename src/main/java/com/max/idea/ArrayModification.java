a96512f8f679cec704d8daee520ec8c0b3d5fabe
Idilled <130499611+Idilled@users.noreply.github.com>
2023-12-09 16:17:21 +0400

    Задача №9_Циклы

---
+++ src/main/java/com/max/idea/ArrayModification.java
@@ -0,0 +1,31 @@
+package com.max.idea;
+
+import java.util.Scanner;
+
+public class ArrayModification {
+    public static void main(String[] args) {
+        int arrayLength;
+        double sum = 0;
+        double arithMean;
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Введите размер массива");
+        arrayLength = input.nextInt();
+        double[] array = new double[arrayLength];
+
+        System.out.println("Введите элементы массива, их должно быть " + arrayLength);
+        for (int i = 0; i < arrayLength; i++) {
+            array[i] = input.nextDouble();
+        }
+
+        for (int i = 0; i < arrayLength; i++) {
+            sum += array[i];
+        }
+        arithMean = sum / (arrayLength);
+
+        System.out.println("Массив, где каждый элемент умножается на среднее арифметическое");
+        for (int i = 0; i < arrayLength; i++) {
+            System.out. println (array[i] * arithMean);
+        }
+    }
+}
