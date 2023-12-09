7d53a90f45fd2d83a60c2ae8fab4aa70e4cf8651
Idilled <130499611+Idilled@users.noreply.github.com>
2023-12-09 14:13:08 +0400

    Задача №8_Циклы

---
+++ src/main/java/com/max/idea/SumNumber.java
@@ -0,0 +1,25 @@
+package com.max.idea;
+
+import java.util.Scanner;
+
+public class SumNumber {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n;
+        int sum = 0;
+
+        System.out.println("Введите любое целое положительное число");
+        n = input.nextInt();
+        if (n < 0) {
+            System.out.println("Пожалуйста, перезапустите программу и введите любое целое положительное число");
+        } else {
+            for (int i = n; i > 0; i--) {
+                if (i % 2 == 0) {
+                    continue;
+                }
+                sum += i;
+            }
+            System.out.println("Сумма всех нечётных чисел от 1 до введенного числа: " + sum);
+        }
+    }
+}
