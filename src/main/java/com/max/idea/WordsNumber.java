1eb512d0156997189f8cb45aae2c614b4214b838
Idilled <130499611+Idilled@users.noreply.github.com>
2023-12-15 21:38:42 +0400

    Задача №13_ Работа с данными

---
+++ src/main/java/com/max/idea/WordsNumber.java
@@ -0,0 +1,25 @@
+package com.max.idea;
+
+import java.util.Scanner;
+
+public class WordsNumber {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String str;
+        int count = 0;
+
+        System.out.println("Введите строку из слов, разделенных пробелами: ");
+        str = input.nextLine();
+        str = str.trim();
+        String[] strArray = str.split(" ");
+
+        for (int i = 0; i < strArray.length; i++) {
+                boolean result = strArray[i].matches("^[\\p{InBasicLatin}]+$"); // ИЛИ regex: "^[\u0000-\u007F]+$"
+                if (result == true) {
+                    System.out.println(strArray[i]);
+                    count++;
+                }
+            }
+        System.out.println("Отобрано " + count + " слов(a), которые содержат только латиницу");
+        }
+}
