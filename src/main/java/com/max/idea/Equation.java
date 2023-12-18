fb0c17e22141a8def7bb5dec6bc1b1be04c2b081
Idilled <130499611+Idilled@users.noreply.github.com>
2023-12-19 01:39:15 +0400

    Итоговая задача №2

---
+++ src/main/java/com/max/idea/Equation.java
@@ -0,0 +1,56 @@
+package com.max.idea;
+
+import java.util.Scanner;
+
+public class Equation {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String equationString;
+        int a;
+        int b;
+        int x;
+
+        System.out.println("Эта программа позволяет решать простые линейные уравнения вида: a+b=c ИЛИ (a-b=c), где два значения - числа, одно значение - неизвестное (x)");
+        System.out.println("Введите уравнение. Пример: x+2=5");
+        equationString = input.next();
+
+        if ((equationString.charAt(1) == '+') && equationString.charAt(3) == '=') {
+            if (Character.isDigit(equationString.charAt(0)) & Character.isDigit(equationString.charAt(2)) & (equationString.charAt(4) == 'x' | (equationString.charAt(4) == 'х'))) {
+                a = equationString.charAt(0);
+                b = equationString.charAt(2);
+                x = a + b;
+                System.out.println("x = " + x);
+            } else if (Character.isDigit(equationString.charAt(0)) & Character.isDigit(equationString.charAt(4)) & (equationString.charAt(2) == 'x' | (equationString.charAt(2) == 'х'))) {
+                a = equationString.charAt(0);
+                b = equationString.charAt(4);
+                x = b - a;
+                System.out.println("x = " + x);
+            } else if (Character.isDigit(equationString.charAt(2)) & Character.isDigit(equationString.charAt(4)) & (equationString.charAt(0) == 'x' | (equationString.charAt(0) == 'х'))) {
+                a = equationString.charAt(2);
+                b = equationString.charAt(4);
+                x = b - a;
+                System.out.println("x = " + x);
+            }
+        } else if ((equationString.charAt(1) == '-') && equationString.charAt(3) == '=') {
+            if (Character.isDigit(equationString.charAt(0)) & Character.isDigit(equationString.charAt(2)) & (equationString.charAt(4) == 'x' | (equationString.charAt(4) == 'х'))) {
+                a = equationString.charAt(0);
+                b = equationString.charAt(2);
+                x = a - b;
+                System.out.println("x = " + x);
+            } else if (Character.isDigit(equationString.charAt(0)) & Character.isDigit(equationString.charAt(4)) & (equationString.charAt(2) == 'x' | (equationString.charAt(2) == 'х'))) {
+                a = equationString.charAt(0);
+                b = equationString.charAt(4);
+                x = a - b;
+                System.out.println("x = " + x);
+            } else if (Character.isDigit(equationString.charAt(2)) & Character.isDigit(equationString.charAt(4)) & (equationString.charAt(0) == 'x' | (equationString.charAt(0) == 'х'))) {
+                a = equationString.charAt(2);
+                b = equationString.charAt(4);
+                x = a + b;
+                System.out.println("x = " + x);
+            }
+        }  else {
+            System.out.println("Введенное уравнение не соответствует схеме. Перезапустите программу и введите уравнение, соответствующее схеме");
+        }
+
+    }
+}
